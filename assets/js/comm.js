1a3cb49a7f78bea79c854da4c53c0428d2484cf2
UDintheroom <dkaqktk38@gmail.com>
2024-02-16 12:32:00 +0900

    intro text edit

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
-  // fullpage
-  $('#fullpage').fullpage({
-    sectionsColor: ['#D6C1FF', '#4BBFC3', '#7BAABE', 'whitesmoke'],
-});
+    // fullpage
+    $('#fullpage').fullpage({
+        sectionsColor: ['#D6C1FF', '#4BBFC3', '#7BAABE', 'whitesmoke'],
+    });
 
-// intro) text fade in
-setTimeout(function() {
-  $(".intro-animation .intro-text").css('display', 'flex');
-}, 3500);
+    // intro) text fade in
+    setTimeout(function() {
+        $(".intro-animation .intro-text").css('display', 'flex');
+        $('.intro-text').addClass('animate__animated animate__rubberBand');
+    }, 3300);
 
-// !!!!!!!never delete!!!!!!!!!!
+    // intro) hello switch
+    $('.kor').mouseover(function() {
+        $(this).css('display', 'none');
+        $('.eng').css('display', 'block');
+    });
+    $('.eng').mouseout(function() {
+        $('.kor').css('display', 'block');
+        $(this).css('display', 'none');
+    });
+
+
+// !!!!!!!!!!! never deldete !!!!!!!!!!
 });
